edd783d7c2b489328d104c237583ed9eddc420b0
oumatoulacen <username@users.noreply.github.com>
2025-05-05 03:31:04 +0100

    fullstackopen part 1 exercises 12 to 14

---
+++ part1/anecdotes/README.md
@@ -0,0 +1,3 @@
+fullstackopen part 1 exercises 12 to 14
+
+using react to create a simple course information app
---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max)
+}
+
+const Button = ({ text, onClick }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+const Anecdote = ({ text }) => {
+  return (
+    <div>
+      {text}
+    </div>
+  )
+}
+const AnecdoteButton = ({ anecdotes, setSelected }) => {
+  const randomAnecdote = () => {
+    const randomIndex = getRandomInt(anecdotes.length)
+    setSelected(randomIndex)
+  }
+
+  return (
+    <Button text="Next Anecdote" onClick={randomAnecdote} />
+  )
+}
+
+const VoteButton = ({ votes, setVotes, selected }) => {
+  const voteAnecdote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
+  return (
+    <Button text="Vote" onClick={voteAnecdote} />
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+
+  return (
+    <div>
+      <Anecdote text={anecdotes[selected]} />
+      <div>has {votes[selected]} votes</div>
+      <VoteButton votes={votes} setVotes={setVotes} selected={selected} />
+      <AnecdoteButton anecdotes={anecdotes} setSelected={setSelected} />
+      
+      <h2>Anecdote with most votes</h2>
+      <Anecdote text={anecdotes[votes.indexOf(Math.max(...votes))]} />
+      <div>has {Math.max(...votes)} votes</div>
+    </div>
+  )
+}
+
+export default App
