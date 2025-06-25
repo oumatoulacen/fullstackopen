3eddc0234cffb3d4c0bb2111fcb1652bb386426a
oumatoulacen <username@users.noreply.github.com>
2025-06-25 13:09:38 +0100

    5.17: Blog List End To End Testing, step 1

---
+++ part5/bloglist-frontend/E2E-testing/tests/login.spec.js
@@ -0,0 +1,13 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('/')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByLabel('Username')).toBeVisible()
+    await expect(page.getByLabel('Password')).toBeVisible()
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
+  })
+})
