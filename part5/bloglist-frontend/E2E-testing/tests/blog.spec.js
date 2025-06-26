83b19dcef04d5da4a64dc03f3bcaccfda1dfef9d
oumatoulacen <username@users.noreply.github.com>
2025-06-26 03:27:39 +0100

    5.20: Blog List End To End Testing, step 4

--- part5/bloglist-frontend/E2E-testing/tests/blog.spec.js
+++ part5/bloglist-frontend/E2E-testing/tests/blog.spec.js
@@ -39,27 +39,48 @@ describe('Blog app', () => {
       await expect(page.getByText('Create New Blog')).not.toBeVisible()
       await expect(page.getByText('Admin logged in')).not.toBeVisible()
     })
-  })
 
+    describe('When logged in', () => {
+      beforeEach(async ({ page }) => {
+        await login(page, 'admin', 'password')
+      })
 
-  describe('When logged in', () => {
-    beforeEach(async ({ page }) => {
-      await login(page, 'admin', 'password')
-    })
+      test('a new blog can be created', async ({ page }) => {
+        await page.getByText('Create New Blog').click()
+        await expect(page.getByText('Create New Blog')).toBeVisible()
+        await page.getByLabel('Title').fill('My new blog')
+        await page.getByLabel('Author').fill('Admin')
+        await page.getByLabel('URL').fill('https://example.com/my-new-blog')
+        await page.getByRole('button', { name: 'Create' }).click()
 
-    test('a new blog can be created', async ({ page }) => {
-      await page.getByText('Create New Blog').click()
-      await expect(page.getByText('Create New Blog')).toBeVisible()
-      await page.getByLabel('Title').fill('My new blog')
-      await page.getByLabel('Author').fill('Admin')
-      await page.getByLabel('URL').fill('https://example.com/my-new-blog')
-      await page.getByRole('button', { name: 'Create' }).click()
+        const notification = page.locator('.notification')
+        await expect(notification).toHaveText('A new blog "My new blog" by Admin added')
 
-      const notification = page.locator('.notification')
-      await expect(notification).toHaveText('A new blog "My new blog" by Admin added')
+        await expect(page.getByText('My new blog Admin View')).toBeVisible()
+        await expect(page.getByRole('button', { name: 'View' })).toBeVisible()
+      })
 
-      await expect(page.getByText('My new blog Admin View')).toBeVisible()
-      await expect(page.getByRole('button', { name: 'View' })).toBeVisible()
+      test('pass', () => {})
+
+      describe('existing blog', () => {
+        beforeEach(async ({ page }) => {
+          await page.getByText('Create New Blog').click()
+          await page.getByLabel('Title').fill('Existing Blog')
+          await page.getByLabel('Author').fill('Admin')
+          await page.getByLabel('URL').fill('https://example.com/existing-blog')
+          await page.getByRole('button', { name: 'Create' }).click()
+
+          await expect(page.getByText('Existing Blog Admin View')).toBeVisible()
+          await expect(page.getByRole('button', { name: 'View' })).toBeVisible()
+        })
+
+        test('can be liked', async ({ page }) => {
+          await page.getByRole('button', { name: 'View' }).click()
+          await expect(page.getByText('Likes: 0 Like')).toBeVisible()
+          await page.getByRole('button', { name: 'Like' }).click()
+          await expect(page.getByText('Likes: 1 Like')).toBeVisible()
+        })
+      })
     })
   })
 })
