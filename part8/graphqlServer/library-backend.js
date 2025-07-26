f1e2dc4ae281e7a3b9aa8418a4f1c5512c390d3c
oumatoulacen <username@users.noreply.github.com>
2025-07-26 23:06:26 +0100

    8.4: Books of an author

--- part8/graphqlServer/library-backend.js
+++ part8/graphqlServer/library-backend.js
@@ -115,7 +115,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String = "Robert Martin"): [Book!]!
     allAuthors: [AuthorDetails!]!
   }
 `;
@@ -124,7 +124,8 @@ const resolvers = {
 	Query: {
 		bookCount: () => books.length,
 		authorCount: () => authors.length,
-		allBooks: () => books,
+		allBooks: (root, args) =>
+			books.filter((book) => book.author === args.author),
 		allAuthors: () =>
 			authors.map((author) => ({
 				name: author.name,
