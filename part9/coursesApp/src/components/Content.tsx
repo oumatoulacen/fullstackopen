18a796e6e32455d7b768a61bf0c14ee86129fc87
oumatoulacen <username@users.noreply.github.com>
2025-08-21 01:32:05 +0100

    Exercise 9.16

--- part9/coursesApp/src/components/Content.tsx
+++ part9/coursesApp/src/components/Content.tsx
@@ -1,14 +1,57 @@
-import { type CourseType } from '../types';
+import { type CoursePart } from '../types';
 
-function Content({ courses }: { courses: CourseType[] }) {
+function Content({ courses }: { courses: CoursePart[] }) {
 	if (!courses) return null;
 	return (
 		<div>
-			{courses.map((course) => (
-				<p key={course.name}>
-					{course.name} {course.exerciseCount}
-				</p>
-			))}
+			{courses.map((course) => {
+				switch (course.kind) {
+					case 'basic':
+						return (
+							<div key={course.name}>
+								<h3>
+									{course.name}
+									{course.exerciseCount}
+								</h3>
+								<i>{course.description}</i>
+							</div>
+						);
+					case 'group':
+						return (
+							<div key={course.name}>
+								<h3>
+									{course.name}
+									{course.exerciseCount}
+								</h3>
+								<p>Project Exercises: {course.groupProjectCount}</p>
+							</div>
+						);
+					case 'background':
+						return (
+							<div key={course.name}>
+								<h3>
+									{course.name} {course.exerciseCount}
+								</h3>
+								<i>{course.description}</i>
+
+								<p>Submit to {course.backgroundMaterial}</p>
+							</div>
+						);
+					case 'special':
+						return (
+							<div key={course.name}>
+								<h3>
+									{course.name} {course.exerciseCount}
+								</h3>
+								<i>{course.description}</i>
+
+								<p>Requirements: {course.requirements.join(', ')}</p>
+							</div>
+						);
+					default:
+						return null;
+				}
+			})}
 		</div>
 	);
 }
